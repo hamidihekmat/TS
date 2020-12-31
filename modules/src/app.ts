09ada9cf4af74ea2f1d9e1ec8d5fbf2a3271e901
Hekmat Hamidi <theonehamidi@gmail.com>
2020-12-31 00:50:52 -0500

    learned about generic types

--- modules/src/app.ts
+++ modules/src/app.ts
@@ -23,14 +23,40 @@ const apple: Product = {
 
 names.push(apple);
 
-const promise = new Promise<string>((resolve, reject) => {
-  setTimeout(() => {
-    resolve('This is done!');
-  }, 2000);
-  return 'Completed';
-});
+// const promise = new Promise<string>((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve('This is done!');
+//   }, 2000);
+//   return 'Completed';
+// });
 
 // Generics
 function merge<T, U>(objA: T, objB: U) {
   return Object.assign(objA, objB);
+}
+
+// Generic with contrains
+function newMerge<T extends object, U extends object>(objA: T, objB: U) {
+  return Object.assign(objA, objB);
+}
+
+interface Length {
+  length: number;
+}
+
+// Creating generic types
+function countAndPrint<T extends Length>(element: T): [T, string] {
+  let description = 'Got no value';
+  if (element.length > 0) {
+    description = `Got ${element.length} value`;
+  }
+  return [element, description];
+}
+
+// Keyof object using extends
+function extractAndConvert<T extends object, U extends keyof T>(
+  obj: T,
+  key: U
+) {
+  return 'Value: ' + obj[key];
 }
